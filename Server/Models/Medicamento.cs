8bd1db5dbb2324a76c344622d90db88552e0c2d8
humberto12345 <humbertogil19.hg@gmail.com>
2023-03-19 16:54:29 -0400

    peque cambios

---
+++ Server/Models/Medicamento.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FarmaciaJH.Server.Model;
+
+public class Medicamento
+{
+
+[Key]
+public int Idmedicamento { get; set; }
+public string Nombre { get; set; }= null!;
+public int Precio { get; set; }
+public string descripcion { get; set; } = null!;
+}
